7d854b010d35d2358f2bf16fb5b64b42011daf6f
Feliverse <114632980+Feliverse@users.noreply.github.com>
2023-12-28 20:06:34 -0400

    Rubocop offenses fixed

--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_12_19_215157) do
+ActiveRecord::Schema[7.0].define(version: 20_231_219_215_157) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
+  enable_extension 'plpgsql'
 
-  create_table "tasks", force: :cascade do |t|
-    t.string "name"
-    t.boolean "completed", default: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+  create_table 'tasks', force: :cascade do |t|
+    t.string 'name'
+    t.boolean 'completed', default: false
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
   end
-
 end
